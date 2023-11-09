37fe27f9576b36f17300a1b2d01806b50f2fd20b
Rakesh <rakesh.h7997@gmail.com>
2023-11-09 16:00:11 +0530

    work on js in line 4

---
+++ Sketch.js
@@ -0,0 +1,8 @@
+console.log("hai")
+document.addEventListener("DOMContentLoaded",function() {
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColums = "repeat(15, 1fr)";
+    board.style.gridTemplateRows = "repeat(15, 1fr)";
+    console.log("hai")
+});
+
