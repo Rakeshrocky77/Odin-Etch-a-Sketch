8c922576174318ef784a75b2306f35eee1b49270
Rakesh <rakesh.h7997@gmail.com>
2023-12-16 13:42:31 +0530

    working wit a buttons

--- Sketch.js
+++ Sketch.js
@@ -1,3 +1,4 @@
+  let color = "black";
 document.addEventListener("DOMContentLoaded",function() {
    createBoard(16);
    
@@ -19,9 +20,7 @@ document.addEventListener("DOMContentLoaded",function() {
 
     for(let i=0; i <numDivs; i++){
         let div = document.createElement("div");
-        div.addEventListener("mouseover",function(){
-            div.style.backgroundColor ="black";
-        })
+        div.addEventListener("mouseover",colorDiv);
         board.insertAdjacentElement("beforeend",div);
     }
 
@@ -40,4 +39,22 @@ function getsize (){
         message.innerHTML = "Now you play";
         return input;
     }
+}
+
+function colorDiv(){
+   if(color == "random"){
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+   }
+   else {
+    this.style.backgroundColor = 'black'
+   }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
 }
